1164223ccc1917a758330c4aee5d53c355e9e13f
NickBwalley <nickbwalley@gmail.com>
2021-05-16 00:23:17 +0300

    Django Project

--- StudentsPortal/App1/views.py
+++ StudentsPortal/App1/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
+
+from django.contrib import messages
 # Create your views here.
 from . forms import CreateUserForm
 
@@ -11,6 +13,9 @@ def register(request):
 		form = CreateUserForm(request.POST)
 		if form.is_valid():
 			form.save()
+			user = form.cleaned_data.get('username') # allows to get only the username without any other details
+			messages.success(request, 'Account successfully created for: ' + user)
+			return redirect('login')
 
 	context = {'form':form}
 	return render(request, 'App1/register.html', context)
