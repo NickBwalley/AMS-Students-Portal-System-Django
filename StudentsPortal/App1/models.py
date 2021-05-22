8a0118add9ef0db5073d5280ea8a72af2c5b7b56
NickBwalley <nickbwalley@gmail.com>
2021-05-22 20:17:01 +0300

    Django Project

--- StudentsPortal/App1/models.py
+++ StudentsPortal/App1/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.core.validators import RegexValidator
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
-
 
 class Country(models.Model):
     name = models.CharField(max_length=40)
@@ -96,3 +97,12 @@ class Profile(models.Model):
 
 	def __str__(self):
 		return self.user.email
+
+@receiver(post_save, sender=user)
+def create_user_profile(sender, instance, created, **kwargs):
+	if created:
+		Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=user)
+def save_user_profile(sender, instance, **kwargs):
+	instance.profile.save()
